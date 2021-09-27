5770b2a5bf2be2077f5ad80a397757beefef16cf
clearsky77 <clearsky3264@gmail.com>
2021-09-27 13:07:24 +0900

    라이브러리2(PhotoView) 1)intent할 xml작성

---
+++ app/src/main/java/com/clearsky77/librarypractice_review/PhotoViewActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.librarypractice_review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_view)
+    }
+}
