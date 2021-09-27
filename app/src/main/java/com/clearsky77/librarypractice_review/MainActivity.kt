489f25d0ba5cd7d3ad97c8e4b0c1d5abe7c9b4fc
clearsky77 <clearsky3264@gmail.com>
2021-09-27 13:30:48 +0900

    라이브러리2(PhotoView) 2)라이브러리 적용 3)intent로 연결 - 사진이 흐리게 보이는 문제 발생

--- app/src/main/java/com/clearsky77/librarypractice_review/MainActivity.kt
+++ app/src/main/java/com/clearsky77/librarypractice_review/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.clearsky77.librarypractice_review
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profilePhotoImg.setOnClickListener {
+            val myIntent = Intent(this, PhotoViewActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
