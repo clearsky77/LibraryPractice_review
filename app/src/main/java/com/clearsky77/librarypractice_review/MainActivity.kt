aaccb25b86335b984950d39f385fbf5759922700
clearsky77 <clearsky3264@gmail.com>
2021-09-28 17:10:46 +0900

    call 기능 구현 - 1. 버튼 클릭시 동작 구현

--- app/src/main/java/com/clearsky77/librarypractice_review/MainActivity.kt
+++ app/src/main/java/com/clearsky77/librarypractice_review/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.clearsky77.librarypractice_review
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,10 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// 사진 클릭시 사진 상세 화면으로 Intent
         profilePhotoImg.setOnClickListener {
             val myIntent = Intent(this, PhotoViewActivity::class.java)
             startActivity(myIntent)
         }
+//        call 버튼 클릭시 전화
+        callBtn.setOnClickListener {
+            val myUri = Uri.parse("tel:${telNumTxt.toString()}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
     }
 }
